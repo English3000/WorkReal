5dc83108cedc35a5dd82adabcbafae2b9c2e21f8
Alexander <english3000.org@gmail.com>
2017-12-10 19:04:12 -0800

    Reals frontend further skeletoned

--- frontend/components/realsIndex.js
+++ frontend/components/realsIndex.js
@@ -4,16 +4,26 @@ import RealFormContainer from 'realFormContainer';
 import Real from './real';
 
 export default class RealsIndex extends React.Component {
+  componentDidMount() {
+    //upon sign-in
+    this.props.getAllReals();
+
+    //upon search query
+
+    //on Role Show Page
+  }
+
   render() {
-    const {reals, roles, projects} = this.props;
+    const {reals, roles, projects, createTruth, deleteTruth} = this.props;
 
     return (<View>
-      <RealFormContainer/>
+      <RealFormContainer/> {/* refactor to RealsIndexPage */}
       {reals && reals.all_ids.length > 0 ? reals.all_ids.map(id => {
         const real = reals.by_id[id];
 
         return <Real key={id} role={roles[real.role_id]}
-                     project={projects[real.project_id]} real={real}/>;
+                     project={projects[real.project_id]} real={real}
+                     truth={createTruth} untruth={deleteTruth}/>;
       }) : null}
     </View>);
   }
