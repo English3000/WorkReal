5862729ac40b941b5c473877e20ab64fbeb0ffa4
Mike Salisbury <michael.salisbury87@gmail.com>
2017-12-13 17:53:42 -0800

    add routes

--- frontend/components/navigator.js
+++ frontend/components/navigator.js
@@ -4,6 +4,9 @@ import { StackNavigator } from 'react-navigation';
 import entryFormContainer from './entryFormContainer';
 import roleFormContainer from './roleFormContainer';
 import roleShowContainer from './roleShowContainer';
+import realContainer from './realContainer';
+import realFormContainer from './realFormContainer';
+
 
 export default StackNavigator({
     home: {screen: entryFormContainer},
@@ -12,5 +15,14 @@ export default StackNavigator({
       path: 'role/roleId',
       screen: roleShowContainer
     },
+    realForm: {
+      path: 'real/projectId',
+      screen: realFormContainer
+    },
+    projectReals: {
+      path: 'projectReals/projectId',
+      screen: realContainer
+    },
+
 
 });
