0f34753a61098e14ec601cb6b145ce5b8554889e
Alexander <english3000.org@gmail.com>
2017-12-14 09:07:24 -0800

    cleaner syntax: role_actions

--- frontend/actions/role_actions.js
+++ frontend/actions/role_actions.js
@@ -7,31 +7,29 @@ export const FOLLOW = 'FOLLOW';
 export const UNFOLLOW = 'UNFOLLOW';
 export const RECEIVE_PROJECT = 'RECEIVE PROJECT';
 
-export const receiveRoles = (roles) => ({type: RECEIVE_ROLES, roles});
-export const receiveRole = (role) => ({type: RECEIVE_ROLE, role });
+export const receiveRoles = roles => ({type: RECEIVE_ROLES, roles});
+export const receiveRole = role => ({type: RECEIVE_ROLE, role });
 export const patchRole = role => ({type: UPDATE_ROLE, role});
 
 export const receiveFollow = details => ({type: FOLLOW, details});
 export const removeFollow = details => ({type: UNFOLLOW, details});
 
-export const receiveProject = (project) => ({type: RECEIVE_PROJECT, project});
+export const receiveProject = project => ({type: RECEIVE_PROJECT, project});
 
 export const fetchRoles = () => dispatch => Api.fetchRoles().then(
   roles => dispatch(receiveRoles(roles)),
   err => dispatch(receiveErrors(err.response.data))
 );
-export const fetchRole = (roleId) => dispatch => Api.fetchRole(roleId).then(
+export const fetchRole = roleId => dispatch => Api.fetchRole(roleId).then(
   role => dispatch(receiveRole(role)),
   err => dispatch(receiveErrors(err.response.data))
 );
 
-export const createRole = (role) => dispatch => (
-  Api.createRole(role)
-  .then(newRole => {
+export const createRole = role => dispatch => Api.createRole(role).then(
+  newRole => {
     dispatch(receiveRole(newRole));
-  return newRole;
-  },
-   err => dispatch(receiveErrors(err.response.data)))
+    return newRole;
+  }, err => dispatch(receiveErrors(err.response.data))
 );
 
 //needs to update role w/o overwriting assoc'd data (past projects & reals)
@@ -48,11 +46,9 @@ export const deleteFollow = (id, roleId) => async (dispatch) => dispatch(
   removeFollow(await Api.deleteFollow(id, roleId))
 );
 
-export const createProject = (project) => dispatch => (
-  Api.createProject(project)
-  .then(newProject => {
+export const createProject = project => dispatch => Api.createProject(project).then(
+  newProject => {
     dispatch(receiveProject(newProject));
     return newProject;
-  },
-  err => dispatch(receiveErrors(err.response.data)))
+  }, err => dispatch(receiveErrors(err.response.data))
 );
