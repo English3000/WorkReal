ac225250fbdb938615a8dded9f3b51a4860465ae
Jack Wu <jackwu350@yahoo.com>
2017-12-14 09:21:24 -0800

    hidden homepage

--- frontend/components/entryForm.js
+++ frontend/components/entryForm.js
@@ -7,36 +7,38 @@ import { TabNavigator } from 'react-navigation';
 
 const SESSION_TOKEN = 'SESSION_TOKEN';
 
+//Experimental features
+import MapView from 'react-native-maps';
 
 
-class CircleButton extends React.Component {
-  constructor(props){
-    super(props);
-    this.handlePress = this.handlePress.bind(this);
-
-  }
-
-  handlePress(){
-    const{session,processForm,credentials} = this.props;
-    //sign in/sign up the user and then access the state/store for updated currentUser and session_token
-    processForm({email: credentials.email,password: credentials.password})
-  }
-
-
-
-
-
-  render() {
-    const {title, style, _onPress} = this.props;
-
-    return (<TouchableOpacity style={style} onPress={this.handlePress}>
-              <Text style={{ height: 85, width: 85, fontWeight: 'bold',
-                              padding: 25, textAlign: 'center',
-                              backgroundColor: 'white',
-                              borderRadius: 100}}>{title}</Text>
-            </TouchableOpacity>);
-  }
-}
+// class CircleButton extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.handlePress = this.handlePress.bind(this);
+//
+//   }
+//
+//   handlePress(){
+//     const{session,processForm,credentials} = this.props;
+//     //sign in/sign up the user and then access the state/store for updated currentUser and session_token
+//     processForm({email: credentials.email,password: credentials.password})
+//   }
+//
+//
+//
+//
+//
+//   render() {
+//     const {title, style, _onPress} = this.props;
+//
+//     return (<TouchableOpacity style={style} onPress={this.handlePress}>
+//               <Text style={{ height: 85, width: 85, fontWeight: 'bold',
+//                               padding: 25, textAlign: 'center',
+//                               backgroundColor: 'white',
+//                               borderRadius: 100}}>{title}</Text>
+//             </TouchableOpacity>);
+//   }
+// }
 
 export default class EntryForm extends React.Component {
   constructor(props) {
@@ -49,134 +51,117 @@ export default class EntryForm extends React.Component {
 
   }
 
-  async componentDidMount() {
-    await Font.loadAsync({
-      'Amaranth': require('../assets/fonts/Amaranth-Regular.ttf'),
-      'FontAwesome': require('../assets/fonts/FontAwesome.otf'),
-    });
-
-    this.setState({ fontLoaded: true });
-
-    // (this.getToken() === true) ? this.props.navigation.navigate(`roleForm`): null;
-    if(this.props.session.currentUser !== null){
-      console.log("Hopefully not here");
-<<<<<<< HEAD
-      AsyncStorage.getItem(SESSION_TOKEN).then((res) => console.log(res));
-=======
-      AsyncStorage.getItem(SESSION_TOKEN).then((res) => console.log("HERE",res));
->>>>>>> c4e5c77db2a5a920932d98a4ecb5e14b3101ded3
-    }
-
-  }
-
-  // async getToken(){
+  // async componentDidMount() {
+  //   await Font.loadAsync({
+  //     'Amaranth': require('../assets/fonts/Amaranth-Regular.ttf'),
+  //     'FontAwesome': require('../assets/fonts/FontAwesome.otf'),
+  //   });
   //
-  //   try {
-  //     let value = await AsyncStorage.getItem(SESSION_TOKEN)
-  //     console.log(value);
-  //     if (value){
+  //   this.setState({ fontLoaded: true });
   //
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
+  //   // (this.getToken() === true) ? this.props.navigation.navigate(`roleForm`): null;
+  //   if(this.props.session.currentUser !== null){
+  //     console.log("Hopefully not here");
+  //     AsyncStorage.getItem(SESSION_TOKEN).then((res) => console.log(res));
   //   }
   //
   // }
 
 
 
-  async storeToken(token){
-    //Write Async storage here
-
-    //Async storage now has an item called 'currentUser' with a value of token.
-    //token === currentUser.session_token
-
-    await AsyncStorage.setItem(SESSION_TOKEN, token).then( () => {
-
-       return AsyncStorage.getItem(SESSION_TOKEN).then((res) => (
-         Alert.alert("HERE",res)
-       ))
-
-    })
-
-  }
 
 
+  // async storeToken(token){
+  //   //Write Async storage here
+  //
+  //   //Async storage now has an item called 'currentUser' with a value of token.
+  //   //token === currentUser.session_token
+  //
+  //   await AsyncStorage.setItem(SESSION_TOKEN, token).then( () => {
+  //
+  //      return AsyncStorage.getItem(SESSION_TOKEN).then((res) => (
+  //        Alert.alert("HERE",res)
+  //      ))
+  //
+  //   })
+  //
+  // }
 
-  componentWillReceiveProps(newProps){
-    // console.log(AsyncStorage.getItem('session_token'))
-    //if currentUser changed
-    if (newProps.session.currentUser !== this.props.session.currentUser) {
-      //When user first logs in store their session_token in AsyncStorage
-      // And then redirect them to roleForm
-            this.storeToken(newProps.session.currentUser.session_token);
-            this.props.navigation.navigate(`roleForm`);
 
-    } else { //currentUser is null
-      Alert.alert('', `${newProps.errors.join('.\n\n')}.`);
-    }
-  }
+
+  // componentWillReceiveProps(newProps){
+  //   // console.log(AsyncStorage.getItem('session_token'))
+  //   //if currentUser changed
+  //   if (newProps.session.currentUser !== this.props.session.currentUser) {
+  //     //When user first logs in store their session_token in AsyncStorage
+  //     // And then redirect them to roleForm
+  //           this.storeToken(newProps.session.currentUser.session_token);
+  //           this.props.navigation.navigate(`roleForm`);
+  //
+  //   } else { //currentUser is null
+  //     Alert.alert('', `${newProps.errors.join('.\n\n')}.`);
+  //   }
+  // }
 
   //should institute 2-factor authentication
-  render() {
+//   render() {
+//
+//     const { navigate } = this.props.navigation;
+//     return (
+//
+//       <View style={styles.view}>
+//
+//         {this.state.fontLoaded ? <View style={styles.upper}>
+//           <Text style={[styles.text, styles.header]}>
+//             <Image source={require('../assets/images/logo.png')} style={{width: 187.5, height: 93.75}}/>
+//           &nbsp;WorkReal</Text>
+//           <View style={{flexDirection: 'row', alignItems: 'center'}}>
+//             <CircleButton title='SIGN UP'
+//                           style={{position: 'relative', right: 3}}
+//                           session={this.props.session}
+//                           processForm={this.props.signUp}
+//                           credentials={ this.state }
+//                           _onPress={() => this.props.signUp({
+//                             email: this.state.email, password: this.state.password
+//                           }).then(() => navigate('roleForm'))}></CircleButton>
+//             <View>
+//               <TextInput style={[styles.text, styles.input]}
+//                          placeholder='email'
+//                          value={this.state.email}
+//                          underlineColorAndroid='transparent' type={'email-address'}
+//                          onChangeText={ email => this.setState({ email }) }/>
+//               <TextInput style={[styles.text, styles.input, styles.lastInput]}
+//                          placeholder='password' secureTextEntry={true}
+//                          underlineColorAndroid='transparent'
+//                          onChangeText={ password => this.setState({ password }) }/>
+//             </View>
+//             <CircleButton title='SIGN IN'
+//                           style={{position: 'relative', left: 3}}
+//                           session={this.props.session}
+//                           processForm={this.props.signIn}
+//                           credentials={this.state}
+//                           _onPress={() => this.props.signIn({
+//                             email: this.state.email, password: this.state.password
+//                           })}></CircleButton>
+//           </View>
+//         </View> : null}
+//       </View>
+//     );
+//
+//   }
+// }
 
-    const { navigate } = this.props.navigation;
-    return (
+  render(){
+    return(
+      <View>
+        <Text> Hello World </Text>
 
-      <View style={styles.view}>
-
-        {this.state.fontLoaded ? <View style={styles.upper}>
-          <Text style={[styles.text, styles.header]}>
-            <Image source={require('../assets/images/logo.png')} style={{width: 187.5, height: 93.75}}/>
-          &nbsp;WorkReal</Text>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <CircleButton title='SIGN UP'
-                          style={{position: 'relative', right: 3}}
-                          session={this.props.session}
-                          processForm={this.props.signUp}
-                          credentials={ this.state }
-                          _onPress={() => this.props.signUp({
-                            email: this.state.email, password: this.state.password
-                          }).then(() => navigate('roleForm'))}></CircleButton>
-            <View>
-              <TextInput style={[styles.text, styles.input]}
-                         placeholder='email'
-                         value={this.state.email}
-                         underlineColorAndroid='transparent' type={'email-address'}
-                         onChangeText={ email => this.setState({ email }) }/>
-              <TextInput style={[styles.text, styles.input, styles.lastInput]}
-                         placeholder='password' secureTextEntry={true}
-                         underlineColorAndroid='transparent'
-                         onChangeText={ password => this.setState({ password }) }/>
-            </View>
-            <CircleButton title='SIGN IN'
-                          style={{position: 'relative', left: 3}}
-                          session={this.props.session}
-                          processForm={this.props.signIn}
-                          credentials={this.state}
-                          _onPress={() => this.props.signIn({
-                            email: this.state.email, password: this.state.password
-                          })}></CircleButton>
-          </View>
-        </View> : null}
       </View>
-    );
-    //else
-    /* return (
-      TabNavigator({
-        Home: {
-          screen: EntryFormContainer,
-          // navigationOptions: { headerTitle: 'Home' }
-        },
-        Details: {
-          screen: EntryFormContainer
-        }
-      });
-    ) */
+
+    )
+
   }
+
 }
 
 const styles = StyleSheet.create({
